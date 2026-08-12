baac8d64d06319e47e93cfc4bc951df8441021b6
Thiago <priotothiago@gmail.com>
2026-08-12 14:53:13 -0300

    fix: enable Scalar API reference in Production environment

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("MedicationManagerDB");
+var connectionString = builder.Configuration.GetConnectionString("MedicationManagerOnlineDB");
 
 builder.Services.AddDbContext<MedicationManagerContext>(options =>
     options.UseSqlServer(connectionString));
@@ -20,11 +20,8 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
     app.MapOpenApi();
     app.MapScalarApiReference();
-}
 
 app.UseHttpsRedirection();
 
